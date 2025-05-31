37dd0d0103b9e32202aa59b1140eabf7e87bb2cb
ducthanhdragonvip <ducthanhdragonvip@gmail.com>
2025-05-31 16:11:56 +0000

    fix(settings): fix some security in settings

--- US-pipeline/config/settings.py
+++ US-pipeline/config/settings.py
@@ -5,45 +5,37 @@ load_dotenv()
 
 class Settings:
     # Kafka Configuration
-    KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS', 'localhost:9092').split(',')
-    APP_TOKEN = os.getenv('APP_TOKEN', 'FG5Z82U02Y2QbLeIADvHAGA85') ## evergreen.data.socrata.com
+    KAFKA_BOOTSTRAP_SERVERS = os.getenv('KAFKA_BOOTSTRAP_SERVERS')
+    APP_TOKEN = os.getenv('APP_TOKEN')
+    GCS_BUCKET = os.getenv('GCS_BUCKET')
 
-    GCS_BUCKET = os.getenv('GCS_BUCKET', 'crime-data-group8')
-
-    
     # Chicago Configuration
-    CHICAGO_SOCRATA_DOMAIN = os.getenv('CHICAGO_SOCRATA_DOMAIN', 'data.cityofchicago.org')
-    CHICAGO_DATASET_ID = os.getenv('CHICAGO_DATASET_ID', 'ijzp-q8t2')
-    CHICAGO_KAFKA_TOPIC = os.getenv('CHICAGO_KAFKA_TOPIC', 'chicago_crimes')
-    CHICAGO_TIMESTAMP_FILE = os.getenv('CHICAGO_TIMESTAMP_FILE', 'data/last_updated.txt')
-    CHICAGO_UPDATE_FIELD = os.getenv('CHICAGO_UPDATE_FIELD', 'updated_on')
-    CHICAGO_ID_FIELD = os.getenv('CHICAGO_ID_FIELD', 'id')
+    CHICAGO_SOCRATA_DOMAIN = os.getenv('CHICAGO_SOCRATA_DOMAIN')
+    CHICAGO_DATASET_ID = os.getenv('CHICAGO_DATASET_ID')
+    CHICAGO_KAFKA_TOPIC = os.getenv('CHICAGO_KAFKA_TOPIC')
+    CHICAGO_TIMESTAMP_FILE = os.getenv('CHICAGO_TIMESTAMP_FILE')
+    CHICAGO_UPDATE_FIELD = os.getenv('CHICAGO_UPDATE_FIELD')
+    CHICAGO_ID_FIELD = os.getenv('CHICAGO_ID_FIELD')
     CHICAGO_OUTPUT_PATH = f"gs://{GCS_BUCKET}/US-chicago/2020To-Present/streaming_output"
     CHICAGO_CHECKPOINT_PATH = f"gs://{GCS_BUCKET}/US-chicago/checkpoints"
 
-
     # Seattle Configuration
-    SEATTLE_SOCRATA_DOMAIN = os.getenv('SEATTLE_SOCRATA_DOMAIN', 'cos-data.seattle.gov')
-    SEATTLE_DATASET_ID = os.getenv('SEATTLE_DATASET_ID', 'tazs-3rd5')
-    SEATTLE_KAFKA_TOPIC = os.getenv('SEATTLE_KAFKA_TOPIC', 'seattle_crimes')
-    SEATTLE_TIMESTAMP_FILE = os.getenv('SEATTLE_TIMESTAMP_FILE', 'data/last_updated_seattle.txt')
-    SEATTLE_UPDATE_FIELD = os.getenv('SEATTLE_UPDATE_FIELD', 'report_date_time')
-    SEATTLE_ID_FIELD = os.getenv('SEATTLE_ID_FIELD', 'report_number')
+    SEATTLE_SOCRATA_DOMAIN = os.getenv('SEATTLE_SOCRATA_DOMAIN')
+    SEATTLE_DATASET_ID = os.getenv('SEATTLE_DATASET_ID')
+    SEATTLE_KAFKA_TOPIC = os.getenv('SEATTLE_KAFKA_TOPIC')
+    SEATTLE_TIMESTAMP_FILE = os.getenv('SEATTLE_TIMESTAMP_FILE')
+    SEATTLE_UPDATE_FIELD = os.getenv('SEATTLE_UPDATE_FIELD')
+    SEATTLE_ID_FIELD = os.getenv('SEATTLE_ID_FIELD')
     SEATTLE_OUTPUT_PATH = f"gs://{GCS_BUCKET}/US-seattle/streaming_output"
     SEATTLE_CHECKPOINT_PATH = f"gs://{GCS_BUCKET}/US-seattle/checkpoints"
 
-    
     # Spark Configuration
-    SPARK_MASTER = os.getenv('SPARK_MASTER', 'local[*]')
-    SERVICE_ACCOUNT_KEY_PATH = os.getenv('SERVICE_ACCOUNT_KEY_PATH', '/home/JT/test-456807-0581efb4fbc8.json')
+    SPARK_MASTER = os.getenv('SPARK_MASTER')
+    SERVICE_ACCOUNT_KEY_PATH = os.getenv('SERVICE_ACCOUNT_KEY_PATH')
     
     # Processing Configuration
-    POLL_INTERVAL = int(os.getenv('POLL_INTERVAL', 3600))  # 1 hour
-    API_LIMIT = int(os.getenv('API_LIMIT', 1000))
-    MAX_RETRIES = int(os.getenv('MAX_RETRIES', 3))
-    
-    
-
-    APP_TOKEN = os.getenv('APP_TOKEN','FG5Z82U02Y2QbLeIADvHAGA85')
+    POLL_INTERVAL = int(os.getenv('POLL_INTERVAL'))
+    API_LIMIT = int(os.getenv('API_LIMIT'))
+    MAX_RETRIES = int(os.getenv('MAX_RETRIES'))
 
 settings = Settings()
